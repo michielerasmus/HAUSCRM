66987c4207dfe238b4312328a4344f7846b07f0a
Michiel Erasmus <michielerasmus@Michiels-MacBook-Air.local>
2025-04-22 21:57:17 +0200

    Fix DATABASES config error

--- HAUSCRM/settings.py
+++ HAUSCRM/settings.py
@@ -40,15 +40,14 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=True),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'your_db_name'),
         'USER': os.getenv('DB_USER', 'your_db_user'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'your_db_password'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
-    )
-}
+    }
 
 
 
